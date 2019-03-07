59e84517467318a85d8664ececd8a02543f3f01b
Shariful Islam <si.nayon@gmail.com>
2019-03-07 13:21:45 +0600

    update

--- selenium_data_download.py
+++ selenium_data_download.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 html_location = os.path.join(BASE_DIR, 'html')
 
-for i in range(20,22):
+for i in range(21,30):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     url = f"https://www.amazon.com/E12SV-Single-Carpeted-Subwoofer-Enclosure/product-reviews/B0013N0UEE/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber={i}"
     print(url)
